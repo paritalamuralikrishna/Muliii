15707911d184ee6d3eb2d84860f52970b7c03f69
paritalamuralikrishna <paritalamuralikrishna.kpd@gmail.com>
2026-08-10 21:59:33 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,12 @@
+class add{
+      public int additionOfTwoNumbers(int a, int b){
+               return a+b;
+      }
+}
+class Main{
+   public static void main(String args[]){
+      add a=new add();
+      int r=a.additionOfTwoNumbers(3,5);
+      System.out.println(r);
+  }
+}
