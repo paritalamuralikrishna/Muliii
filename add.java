5dd33762eec3689590056ccef7d704abd4c0a234
Praharsha Krishna Mallupeddi <praharshakrishnamallupeddi@gmail.com>
2026-08-15 15:07:23 +0530

    ThreeNumSub

--- add.java
+++ add.java
@@ -2,6 +2,9 @@ class add{
       public int additionOfTwoNumbers(int a, int b){
                return a+b;
       }
+      public int additionOfThreeNumbers(int a, int b, int c){
+        return a+b+c;
+    }
 }
 class Main{
    public static void main(String[] args){
